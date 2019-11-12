6879ea7893f993572bfd1aae9d883dc544897dfb
Anes Hasicic <me@anes.io>
2019-11-12 22:24:57 +0100

    Added get-event

--- src/eventure/app.clj
+++ src/eventure/app.clj
@@ -1,13 +1,16 @@
 (ns eventure.app
-  (:require [eventure.domain :as domain])
+  (:require [eventure.domain :as d])
   (:import (java.util UUID)))
 
-(defn create-event [save-event cmd]
+(defn create-event [save-to-db cmd]
   (let [id (UUID/randomUUID)
         details (:event-details cmd)
         items (:event-items cmd)
-        event (domain/create-event id details items)]
-    (save-event event)
+        event (d/create-event id details items)]
+    (save-to-db event)
     event))
 
+(defn get-event [get-from-db id]
+  (let [event (get-from-db id)]
+    (dissoc event ::d/event-admin-hash)))
 
